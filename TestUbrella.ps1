2cbea595914c445f8004c0b707c5735d2d65b3b7
Artyom <root@itsec.pro>
2021-10-04 13:57:59 +0200

    Create TestUbrella.ps1

---
+++ TestUbrella.ps1
@@ -0,0 +1,7 @@
+# Umbrella test URLs https://support.umbrella.com/hc/en-us/articles/230903728-How-To-Successfully-test-to-ensure-you-re-running-Umbrella-correctly
+www.internetbadguys.com
+www.examplemalwaredomain.com
+malware.opendns.com
+www.examplebotnetdomain.com
+www.exampleadultsite.com
+Resolve-DnsName -Name $name
